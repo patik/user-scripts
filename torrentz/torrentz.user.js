81fa0c1927066dec17ec08a6af03ee6b9545f25c
Craig Patik <craig@patik.com>
2014-03-09 11:12:15 -0400

    Moved Twitter Avatar to its own repo

---
+++ torrentz/torrentz.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name           Torrentz.eu Customized
+// @description    Links to torrent sites open in the same window, and kickass.to is automatically chosen if available
+// @namespace      http://patik.com/code/user-scripts/
+// @match          https://torrentz.eu/*
+// @match          http://torrentz.eu/*
+// @version        1.0.0
+// @lastupdated    2013-11-18
+// ==/UserScript==
+
+(function _torrentz_customized() {
+    // Find all links to other torrent sites
+    [].slice.call(document.querySelectorAll('a[rel="e"]')).forEach(function(a) {
+        var linkTitle;
+
+        // Make them open in the same window
+        a.removeAttribute('target');
+
+        // Auto navigate to kickass.to
+        linkTitle = a.querySelector('.u');
+        if (linkTitle && linkTitle.innerHTML === 'kickass.to') {
+            window.location.href = a.href;
+        }
+    });
+}());
